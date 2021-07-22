44a7d7c379f449d48ed194be087f7b84c0cd1176
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-22 13:44:11 +0700

    initialization serverBlog

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT_SERVER;
+const userRoute = require("./routes/user.routes");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./database");
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "from index api" });
+});
+
+app.use(userRoute);
+
+app.listen(port, () => {
+  console.log("Server is running ... ");
+});
