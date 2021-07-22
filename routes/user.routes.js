6c13e1e6f0d30e0b54ef078bf2562433154c7095
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-22 18:09:53 +0700

    Sign In with JWT

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const route = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
 const User = require("../models/user.model");
 
 route.post("/user/signup", async (req, res) => {
@@ -14,14 +16,14 @@ route.post("/user/signup", async (req, res) => {
     return res.status(400).json({ error: "Can't blank password" });
   }
 
-  const CreateAccount = () => {
+  const createAccount = () => {
     bcrypt.hash(password, 10).then((hashedPassword) => {
       const user = new User({
         username,
         password: hashedPassword,
       });
       user.save().then(() => {
-        res.status(201).json({ message: "Signup success" });
+        res.status(201).json({ message: "Sign Up success!" });
       });
     });
   };
@@ -29,10 +31,32 @@ route.post("/user/signup", async (req, res) => {
   const checkExitUsername = await User.findOne({ username: username });
 
   if (checkExitUsername) {
-    return res.status(400).json({ error: "User already exits" });
+    return res.status(400).json({ error: "Opps.. User already exits!" });
   } else {
-    CreateAccount();
+    createAccount();
   }
+});
+
+route.post("/user/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  const checkAccountLogin = () => {
+    User.findOne({username: username}).then((savedUser)=>{
+      if(!savedUser){
+        return res.status(400).json({ error: "Opps.. User already not exits" });
+      }
+        bcrypt.compare(password,savedUser.password).then((doMatch)=>{
+          if(doMatch){
+            const token = jwt.sign({ _id: savedUser._id }, process.env.SERECT_KEY,{ expiresIn: '1h' });
+            return res.status(200).json({token});
+          }
+          else return res.status(400).json({ error: "Opps.. Password is not correct" });
+      })
+    })
+  }
+
+  checkAccountLogin();
+
 });
 
 module.exports = route;
