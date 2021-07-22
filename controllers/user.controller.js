b3ef2c5157625d7efe512b54a235ee31e8292bd7
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-23 06:53:16 +0700

    Separate handle logic from routes to controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,50 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+exports.createAccount = async (username, password, res) => {
+  const checkExitsUsername = await User.findOne({ username: username });
+
+  if (checkExitsUsername) {
+    return res.status(400).json({ error: "Opps.. User already exits!" });
+  } else {
+    bcrypt.hash(password, 10).then((hashedPassword) => {
+      const user = new User({
+        username,
+        password: hashedPassword,
+      });
+      user.save().then(() => {
+        res.status(201).json({ message: "Sign Up success!" });
+      });
+    });
+  }
+};
+
+exports.loginAccount = (username, password, res) => {
+  User.findOne({ username: username }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(400).json({ error: "Opps.. User already not exits" });
+    }
+    bcrypt.compare(password, savedUser.password).then((doMatch) => {
+      if (doMatch) {
+        const token = jwt.sign({ _id: savedUser._id }, process.env.SERECT_KEY, {
+          expiresIn: "1h",
+        });
+        return res.status(200).json({ token });
+      } else
+        return res
+          .status(400)
+          .json({ error: "Opps.. Password is not correct" });
+    });
+  });
+};
+
+exports.validatorField = (username, password, res) => {
+  if (!username && !password) {
+    return res.status(400).json({ error: "Can't blank username and password" });
+  } else if (!username) {
+    return res.status(400).json({ error: "Can't blank username" });
+  } else if (!password) {
+    return res.status(400).json({ error: "Can't blank password" });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,62 +1,26 @@
 const express = require("express");
 const route = express.Router();
-const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const User = require("../models/user.model");
+const Authentication = require("../middleware/Authentication");
+const userController = require("../controllers/user.controller");
 
 route.post("/user/signup", async (req, res) => {
   const { username, password } = req.body;
 
-  if (!username && !password) {
-    return res.status(400).json({ error: "Can't blank username and password" });
-  } else if (!username) {
-    return res.status(400).json({ error: "Can't blank username" });
-  } else if (!password) {
-    return res.status(400).json({ error: "Can't blank password" });
-  }
-
-  const createAccount = () => {
-    bcrypt.hash(password, 10).then((hashedPassword) => {
-      const user = new User({
-        username,
-        password: hashedPassword,
-      });
-      user.save().then(() => {
-        res.status(201).json({ message: "Sign Up success!" });
-      });
-    });
-  };
-
-  const checkExitUsername = await User.findOne({ username: username });
-
-  if (checkExitUsername) {
-    return res.status(400).json({ error: "Opps.. User already exits!" });
-  } else {
-    createAccount();
-  }
+  userController.validatorField(username, password, res);
+  userController.createAccount(username, password, res);
 });
 
 route.post("/user/signin", async (req, res) => {
   const { username, password } = req.body;
 
-  const checkAccountLogin = () => {
-    User.findOne({username: username}).then((savedUser)=>{
-      if(!savedUser){
-        return res.status(400).json({ error: "Opps.. User already not exits" });
-      }
-        bcrypt.compare(password,savedUser.password).then((doMatch)=>{
-          if(doMatch){
-            const token = jwt.sign({ _id: savedUser._id }, process.env.SERECT_KEY,{ expiresIn: '1h' });
-            return res.status(200).json({token});
-          }
-          else return res.status(400).json({ error: "Opps.. Password is not correct" });
-      })
-    })
-  }
+  userController.validatorField(username, password, res);
+  userController.loginAccount(username, password, res);
+});
 
-  checkAccountLogin();
-
+route.get("/user/me", Authentication, (req, res) => {
+  console.log(req.user);
+  res.send(req.user);
 });
 
 module.exports = route;
