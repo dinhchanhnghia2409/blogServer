09beb35b7bddf96678e9916ed70886c434d1475c
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-25 12:23:26 +0700

    refactor code server

--- database.js
+++ database.js
@@ -1,14 +1,19 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const addressDatabase = process.env.DATABASE;
-mongoose
-  .connect(addressDatabase, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Server connect Database in ... " + addressDatabase))
-  .catch((errors) => {
-    console.log("Connect with Database Fail. Reason: " + errors);
-  });
+
+exports.connectDatabse = () => {
+  mongoose
+    .connect(addressDatabase, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() =>
+      console.log("Server connect Database in ... " + addressDatabase)
+    )
+    .catch((errors) => {
+      console.log("Connect with Database Fail. Reason: " + errors);
+    });
+};
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ require("dotenv").config();
 const port = process.env.PORT_SERVER;
 const userRoute = require("./routes/user.routes");
 const postRoute = require("./routes/post.routes");
+const database = require("./database");
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require("./database");
+// require("./database");
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Message from server" });
@@ -19,8 +20,13 @@ app.get("/", (req, res, next) => {
 app.use(userRoute);
 app.use(postRoute);
 
-app.listen(port, () => {
-  console.log("Server is running ... ");
-});
+const runServer = async (port) => {
+  await database.connectDatabse();
+  await app.listen(port, () => {
+    console.log("Server is running ... ");
+  });
+};
+
+runServer(port);
 
 module.exports = app;
