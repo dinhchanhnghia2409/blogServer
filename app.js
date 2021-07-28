48829777770b4410591a3b52b53d3bf908c1d70b
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-28 12:07:23 +0700

    change name server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.get("/", (req, res, next) => {
+app.get("/",(req, res, next) => {
   res.json({ message: "Message from server" });
   next()
 });
