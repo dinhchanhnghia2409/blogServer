f61cdde2731a49d4b55e58ba5034f011c4fc6c22
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-07-29 10:35:28 +0700

    add swagger document api

--- app.js
+++ app.js
@@ -6,16 +6,41 @@ const port = process.env.PORT_SERVER;
 const userRoute = require("./routes/user.routes");
 const postRoute = require("./routes/post.routes");
 const database = require("./database");
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Blog Api",
+      description: "Blog Api",
+      contact: {
+        name: "dinhchanhnghia2409@gmail.com"
+      },
+      servers: ["http://localhost:8080"]
+    }
+  },
+  
+  apis: ['./routes/*.js']
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-app.get("/",(req, res, next) => {
-  res.json({ message: "Message from server" });
-  next()
+
+ app.get('/', (req, res, next) => {
+  res.send('Server say hello....!');
+  next();
 });
+
+
 
 app.use(userRoute);
 app.use(postRoute);
